0287571d7dc2feedfb7f3ca84436ed3f7e5bcd69
abdulazeezcoding <abdulazizmohammed5103@gmail.com>
2024-04-15 19:24:30 +0000

    login & signup pages creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StudentLoginPage from './pages/student-loginpage';
+import StudentSignupPage from './pages/student-signuppage';
+import TeacherLoginPage from './pages/teacher-loginpage';
+import TeacherSignupPage from './pages/teacher-signuppage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<StudentLoginPage />} />
+      <Route path='/studentsignup' element={<StudentSignupPage /> }/>
+      <Route path='/teacherlogin' element={<TeacherLoginPage />} />
+      <Route path='/teachersignup' element={<TeacherSignupPage />} />
+    </Routes>
+   </BrowserRouter>
+   </>
   );
 }
 
---
+++ src/pages/student-signuppage.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import student from '../assets/images/student.png';
+import { Link } from 'react-router-dom';
+
+export default function StudentSignupPage() {
+  return (
+<section class="bg-blue-500 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-bold text-white mt-6 dark:text-white">
+          <img class="w-12 h-12 mr-2 rounded" src={student} alt="logo" />
+          MIS WEB    
+      </a>
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-2xl font-bold leading-tight tracking-tight text-blue-500 md:text-2xl dark:text-white">
+                Register (Student) 
+              </h1>
+            
+              <form class="space-y-4 md:space-y-6" action="#">
+              <div class='flex justify-between'>
+                <div>
+                      <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                      <input type="text" name="firstname" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required="" />
+                  </div>
+                  <div>
+                      <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                      <input type="text" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required="" />
+                  </div>
+                  </div>
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Johndoe@st.edu.gh" required="" />
+                  </div>
+                  <div class='flex justify-between'>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                  </div>
+                  <div>
+                      <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                  </div>
+                  </div>
+                 
+                  {/* <div class="flex items-start">
+                      <div class="flex items-center h-5">
+                        <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                      </div>
+                      <div class="ml-3 text-sm">
+                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                      </div>
+                  </div> */}
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
+                      Already have an account? <Link to="/" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                  </p>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
+                      Are you a Teacher? <Link to="/teachersignup" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Register here</Link>
+                  </p>
+
+              </form>
+          </div>
+      </div>
+  </div>
+</section>  );
+}
